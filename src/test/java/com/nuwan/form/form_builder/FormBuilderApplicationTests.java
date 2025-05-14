4ddccb828d020f1d914a3ae1eb1b39a60b447457
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-05-14 23:27:16 +0100

    Initial project configuration in github

---
+++ src/test/java/com/nuwan/form/form_builder/FormBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuwan.form.form_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
